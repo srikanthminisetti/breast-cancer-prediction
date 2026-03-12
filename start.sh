69a7ff1a0de236c5e2280341c54b931fd99dddb7
srikanthminisetti <162618582+srikanthminisetti@users.noreply.github.com>
2026-03-13 00:27:10 +0530

    install numpy before requirements

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-pip install -r requirements-base.txt
+pip install --upgrade pip
+pip install numpy==1.26.4
+
 pip install -r requirements.txt
 
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn app:app --host 0.0.0.0 --port 8000
